7da44c962466dccb1316ca24d209119f6b902487
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-19 12:48:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun (string = "roller-skate" ) {
+    return `This Saturday, I want to ${string}!`
+} 
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork (action = "go to the office"){
+    return `This Monday, I will ${action}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective (adjectives = '*'){
+    return function (inner = "special"){
+        
+
+
+      return `You are ${adjectives} ${inner} ${adjectives}!` 
+
+
+    };
+}
+wrapAdjective()();
